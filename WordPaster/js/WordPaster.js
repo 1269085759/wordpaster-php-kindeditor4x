7c72699a785987a74ba057d44e7fe270a0db7a3a
xfl <xfl@ncmem.com>
2017-09-20 15:36:40 +0800

    load_complete取消隐藏操作

--- WordPaster/js/WordPaster.js
+++ WordPaster/js/WordPaster.js
@@ -600,7 +600,7 @@ function WordPasterManager()
             }
         }
         if (needUpdate) this.need_update();
-        else { $.skygqbox.hide(); }
+        //else { $.skygqbox.hide(); }
     };
     this.recvMessage = function (msg)
 	{
